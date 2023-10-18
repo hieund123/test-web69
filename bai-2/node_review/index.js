d5337686ed126a2564cb471f7915a7a736a40e6b
hieund123 <butbisanh@gmail.com>
2023-10-18 13:35:32 +0700

    test

--- bai-2/node_review/index.js
+++ bai-2/node_review/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const { connectToDb, db } = require("./db");
 
 const app = express();
-
+// dtesting
 app.listen(3000, () => {
   console.log("App is running at 3000");
   connectToDb();
